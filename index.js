e2de72f51fa37e212a7e5bdcacb5b21658eaf78a
Priya Kumari <priya.kumari4122004@gmail.com>
2025-08-21 14:55:33 +0530

    first commit

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const { router } = require('./routes/allroutes')
+const path = require('path')
 require('dotenv').config()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: `${process.env.BASE_URL}`
+}))
+app.use(express.static(path.join(__dirname, 'dist')))
 
+app.get('*', (res,res)=>{
+    res.sendile(path.join(__dirname, 'dist', 'index.html'))
+})
 
 mongoose.connect(process.env.dburl).then(() => {
     console.log('db connected')
